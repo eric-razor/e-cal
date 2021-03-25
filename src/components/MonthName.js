3ea2b07ac92ae2441e3e5a3131553128ea79a234
eric <eric.razorjr@gmail.com>
2021-03-24 22:05:48 -0400

    Create listeners for navigation

--- src/components/MonthName.js
+++ src/components/MonthName.js
@@ -4,9 +4,9 @@ function MonthName(){
     return(
         <thead className="month-name-header">
             <tr className="month-name-table-row">
-                <td><span>{"<"}</span></td>
+                <td className="directional-span" id="left" align="left" onClick={previousMonth} ><span>{"<"}</span></td>
                 <td className="month-name-table-data" colSpan="5" align="center">{currentMonth()}</td>
-                <td align="right"><span>{">"}</span></td>
+                <td className="directional-span" id="right" align="right" onClick={nextMonth}><span>{">"}</span></td>
             </tr>
             
         </thead>
@@ -17,6 +17,17 @@ function currentMonth(){
     const data = new Date();
     let month = data.toLocaleString('default', { month: 'long' });
     return month;
+}
+
+function previousMonth(){
+    console.log("previous month")
+}
+
+function nextMonth(){
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let data = new Date();
+    let currentMonth= data.getMonth()
+    console.log(months[currentMonth + 1])
 }
 
 export default MonthName;
